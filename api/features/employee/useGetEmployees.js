8a6747d27f9b1e2a64f6ed1bea88f91662aa7ea0
chazi13 <m.chairul669@gmail.com>
2022-10-06 00:57:15 +0700

    chore: remove employee

--- api/features/employee/useGetEmployees.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-
-import { axios } from "../../../lib/axios";
-
-export const getEmployees = () =>
-  axios.get(`/employees`).then((res) => res.data);
-
-export const useGetEmployees = () =>
-  useQuery(["getEmployees"], () => getEmployees());
